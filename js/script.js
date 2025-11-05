3d93f39ef68789aef9b07c755ce5a64924d3f07d
Beray Tyuleolu <bsezgin00@gmail.com>
2025-11-05 14:40:37 +0200

    Added a testimonal slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const tnslider = tns ({
+    container: '.slider',
+    slideBy: 1,
+    items: 1,
+    speed:700,
+    nav:true,
+    navPosition: 'bottom',
+    autoPlay:true,
+    autoPlayTimeout:7000,
+    autoPlayButtonOutput:false,
+    controlsContainer: "#controls",
+    prevButton: ".previous",
+    nextButton: ".next",
+});
